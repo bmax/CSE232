c3c40305fab7c4aff34d3b68377d48d5b41aad7a
Brandon Max <maxbrand@msu.edu>
2014-02-24 01:39:34 -0500

    Project 5 Revision

--- proj05/madlib.cpp
+++ proj05/madlib.cpp
@@ -26,29 +26,6 @@ using std::ifstream;
 using std::ofstream;
 using std::istringstream;
 /*
- * Function accepts a string which is a filename
- * opens filename and splits up by new lines into vector
- * returns vector
- */
-vector<string> load_word_file(string filename) {
-  string line;
-  vector<string> t;
-  ifstream wordfile(filename);
-  while (getline(wordfile,line,'\n')) {
-    t.push_back(line);
-  }
-  return t;
-}
-/*
- * Function accepts a reference to a vector and a random generate engine 
- * returns a random word from a vector 
- * returns string 
- */
-string random_word(vector<string> &v, default_random_engine &dre) {
-  uniform_int_distribution<long> dist(0,v.size()-1);
-  return v.at(dist(dre));
-}
-/*
  * Function accepts a string and a reference to a vector
  * splits string by white space delimiter and puts it into vector
  * returns void 
@@ -61,6 +38,39 @@ void split(string text, vector<string> &v) {
   }
 }
 /*
+ * Function accepts a string which is a filename
+ * opens filename and splits up by new lines into vector
+ * returns vector
+ */
+vector<string> load_word_file(string filename) {
+  string line;
+  vector<string> t;
+  ifstream wordfile(filename);
+  while (getline(wordfile,line,'\n')) {
+    split(line,t);
+  }
+  return t;
+}
+vector<string> load_in_file(string filename) { 
+	 ifstream tmpfile(filename);
+  string line;
+	vector<string> templatefile;
+  while (getline(tmpfile,line,'\n')) {
+    split(line,templatefile);
+    templatefile.push_back("\n");
+  }
+	return templatefile;
+}
+/*
+ * Function accepts a reference to a vector and a random generate engine 
+ * returns a random word from a vector 
+ * returns string 
+ */
+string random_word(vector<string> &v, default_random_engine &dre) {
+  uniform_int_distribution<long> dist(0,v.size()-1);
+  return v.at(dist(dre));
+}
+/*
  * Function accepts four string, one int
  * main function that loads noun, verb, in_file and chooses random words and inserts them into out_file
  * seed is used for random engine generator
@@ -69,14 +79,8 @@ void split(string text, vector<string> &v) {
 void process_document(string noun_file, string verb_file, string in_file, string out_file, int seed = 98765) {
   vector<string> nouns = load_word_file(noun_file);
   vector<string> verbs = load_word_file(verb_file);
-  vector<string> templatefile; 
+  vector<string> templatefile = load_in_file(in_file); 
   default_random_engine engine(seed);
-  ifstream tmpfile(in_file);
-  string line;
-  while (getline(tmpfile,line,'\n')) {
-    split(line,templatefile);
-    templatefile.push_back("\n");
-  }
   for ( auto &ele : templatefile) {
     string rand_noun = random_word(nouns,engine); 
     string rand_verb = random_word(verbs,engine); 
