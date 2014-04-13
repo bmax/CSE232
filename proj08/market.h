91c266428dd7cb7c69799699ea3e106efc7a3d9f
bmax <bmax1337@gmail.com>
2014-04-13 14:25:01 -0400

    Project 10 completed.

--- proj08/market.h
+++ proj08/market.h
@@ -20,22 +20,22 @@ using std::string;
 struct Market {
   map<long,vector<double>> stocks;
   vector<string> symbols = {"AA","AXP","BA","BAC","CAT","CSCO","CVX","DD","DIS","GE","HD","HPQ","IBM","INTC","JNJ","JPM","KFT","KO","MCD","MMM","MRK","MSFT","PFE","PG","T","TRV","UTX","VZ","WMT","XOM"};
-/*
- * constructor function
- * returns void 
- */
+  /*
+   * constructor function
+   * returns void 
+   */
   Market() = default;
-/*
- * constructor function
- * takes stock file and reads through it and puts it into stocks map
- * returns void 
- */
+  /*
+   * constructor function
+   * takes stock file and reads through it and puts it into stocks map
+   * returns void 
+   */
   Market(string filename) ;
-/*
- * get price function
- * takes stock and date and returns price if exsists.
- * returns double
- */
+  /*
+   * get price function
+   * takes stock and date and returns price if exsists.
+   * returns double
+   */
   double get_price(string stock, long date);
 
 };
